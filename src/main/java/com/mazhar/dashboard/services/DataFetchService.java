6be61df40fd2ee2399798b4970d34223fda71fc5
mazhar <mazhar.zahur@dohatec.com.bd>
2022-09-29 12:54:44 +0600

    update

--- src/main/java/com/mazhar/dashboard/services/DataFetchService.java
+++ src/main/java/com/mazhar/dashboard/services/DataFetchService.java
@@ -3,8 +3,9 @@ package com.mazhar.dashboard.services;
 import com.mazhar.dashboard.model.CricInfoDataModel;
 import com.mazhar.dashboard.model.Root;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -15,11 +16,11 @@ import javax.xml.bind.Unmarshaller;
 import java.io.StringReader;
 
 @Service
+@EnableScheduling
 @RequiredArgsConstructor
 public class DataFetchService {
     private static final String url = "http://static.cricinfo.com/rss/livescores.xml";
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     public CricInfoDataModel fetchData() {
         try {
@@ -38,5 +39,9 @@ public class DataFetchService {
         return null;
     }
 
+    @Scheduled(cron = "0 */5 * * * *" ,zone="GMT+6")
+    public void startTaskSchedule() {
+
+    }
 
 }
